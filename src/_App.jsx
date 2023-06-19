67db4b2d50fc2a9193a66cf61407e72d10af204c
Renata Vieira <rrenattavs@gmail.com>
2023-06-19 15:20:44 -0600

    Trying Mui - MaterialTable

---
+++ src/_App.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Table from "./Components/Material.jsx";
+
+function App() {
+  
+  return (
+    <div className="App">
+        <h1>Table here</h1>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
